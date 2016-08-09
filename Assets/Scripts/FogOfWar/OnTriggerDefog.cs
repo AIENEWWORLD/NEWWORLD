bd7487e0e879327c2477c218dbe9d1fbaf065105
AIE\blake.rudderham <blakerudderham@gmail.com>
2016-08-09 14:25:12 +1000

    Added OnTriggerDefog ---> Script used for Vista Points; to use add  script "FogOfWarUnit" to component object followed by OnTriggerDefog. Any "Player" object that interacts with gameObject will then trigger deFog in Radius. Radius on gameObject controlled through "Radius" on FogOfWarUnit component.

---
+++ Assets/Scripts/FogOfWar/OnTriggerDefog.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTriggerDefog : MonoBehaviour
+{
+    //Component requires FogOfWarUnit
+    private bool hasBeenTrigger = false;
+
+    void Start()
+    {
+        gameObject.GetComponent<FogOfWarUnit>().enabled = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == ("Player") && hasBeenTrigger == false)
+        {
+            gameObject.GetComponent<FogOfWarUnit>().enabled = true;
+            hasBeenTrigger = true;
+        }
+ 
+    }
+
+
+}
