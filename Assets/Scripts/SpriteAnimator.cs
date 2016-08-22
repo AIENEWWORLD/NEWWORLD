a3036437dd67e90a166b38b80c10ab0d7d624848
AIE\blake.rudderham <blakerudderham@gmail.com>
2016-08-23 09:51:12 +1000

    Added SpriteAnimator script; requires component "Sprite Renderer" - > TextureType must be changed to (Sprite 2D and UI) on each texture. Size of animation must be added in Array form; then each frame added in order. Lastly "FrameInterval" changes animation speed.

--- Assets/Scripts/SpriteAnimator.cs
+++ Assets/Scripts/SpriteAnimator.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpriteAnimator : MonoBehaviour
 {
-    public Material[] AnimatedFrames;
+    public Sprite[] AnimatedFrames;
     Renderer thisObject;
     public float FrameInterval;
     private int NumberOfFrames;
     private int CurrentFrameIndex;
-    private Material thisMaterial;
+    private Sprite thisMaterial;
 
     private float t_Time;
 
@@ -39,7 +40,7 @@ public class SpriteAnimator : MonoBehaviour
                 CurrentFrameIndex += 1;
 
             }
-            thisObject.material = AnimatedFrames[CurrentFrameIndex];
+            thisObject.GetComponent<SpriteRenderer>().sprite = AnimatedFrames[CurrentFrameIndex];
             t_Time = 0.0f;
         }
     }
