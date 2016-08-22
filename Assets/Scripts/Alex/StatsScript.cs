7e4bff5b4bc9795305b89fce9de6dfb9ec8bccbc
AIE\alex.fitzgerald <affar3434@hotmail.com>
2016-08-22 16:16:15 +1000

    Added new combat system into pitch 3 scene.

---
+++ Assets/Scripts/Alex/StatsScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsScript : MonoBehaviour
+{
+    public int maxHealth;
+    public int health;
+    public int attack_coins;
+    public int defence_coins;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
