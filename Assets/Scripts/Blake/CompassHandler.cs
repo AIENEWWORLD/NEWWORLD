f159bdc4ed630f2c574fd62cc0e98026ddb9209c
AIE\alex.fitzgerald <affar3434@hotmail.com>
2016-10-25 16:56:37 +1100

    messed with compass

--- Assets/Scripts/Blake/CompassHandler.cs
+++ Assets/Scripts/Blake/CompassHandler.cs
@@ -5,7 +5,7 @@ public class CompassHandler : MonoBehaviour
 {
     public GameObject LookAtGameOject;
     public GameObject PlayerObject;
-
+    public GameObject CompassGO;
 
 
     //PH
@@ -17,7 +17,9 @@ public class CompassHandler : MonoBehaviour
 
         //transform.position = CurrentPosition.position - transform.position;
 
-        transform.LookAt(LookAtGameOject.transform.position, Vector3.up);
-        transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation((new Vector3(LookAtGameOject.transform.position.x, transform.position.y, LookAtGameOject.transform.position.z) - new Vector3(PlayerObject.transform.position.x, transform.position.y, PlayerObject.transform.position.z))), 100 * Time.smoothDeltaTime);
+
+        //transform.LookAt(LookAtGameOject.transform.position, Vector3.up);
+        //transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
     }
 }
