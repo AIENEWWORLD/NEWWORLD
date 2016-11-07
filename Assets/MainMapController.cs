a4d59e059c809fc27b82e4c25be020aa6e0c38ee
blake <blakerudderham@gmail.com>
2016-11-08 10:03:28 +1100

    Map can now be zoomed in and out with mouse wheel, map can be panned around with mapped keys(currently wasd)

---
+++ Assets/MainMapController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMapController : MonoBehaviour
+{
+    public float ScrollSpeed;
+    [HideInInspector]
+    public GameObject m_player;
+
+    Vector3 StartPositionOfCamera;
+
+    bool mapEnabled = false;
+    float CameraYDistance = 0;
+    float ZoomDistance = 0;
+
+    Camera mapCamera;
+    GameObject InputGameobject;
+    float xVelocity;
+    float zVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartPositionOfCamera = gameObject.transform.position;
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        mapCamera = gameObject.GetComponent<Camera>();
+        CameraYDistance = mapCamera.transform.position.y;
+        ZoomDistance = CameraYDistance;
+        InputGameobject = GameObject.FindGameObjectWithTag("SaveAcrossScenes");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mapCamera.enabled == false)
+        {
+            transform.position = new Vector3(m_player.transform.position.x, CameraYDistance, m_player.transform.position.z);
+        }
+        else if (mapCamera.enabled == true)
+        {
+            ZoomDistance = Input.GetAxis("Mouse ScrollWheel");
+            ZoomDistance = ZoomDistance * -100;
+            xVelocity = InputGameobject.GetComponent<SavedInput>().horizontal;
+            zVelocity = InputGameobject.GetComponent<SavedInput>().vertical;
+
+            transform.position += new Vector3(xVelocity * ScrollSpeed * Time.deltaTime, ZoomDistance, zVelocity * ScrollSpeed* Time.deltaTime);
+
+       }
+
+
+    }
+}
+
